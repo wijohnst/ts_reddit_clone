61bb69aed5dcce358a36a6585a97055404206f26
Michael McBride <Michaelmcbridefilms@gmail.com>
2023-02-13 17:22:21 -0500

    chore: removed boilerplate from fe web folder

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -2,24 +2,10 @@ import React from 'react'
 import { Link } from 'ui'
 import './App.css'
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1 className="header">
-          Web
-          <div className="Turborepo">Turborepo Example</div>
-        </h1>
-        <div>
-          <Link className="App-link" href="https://turbo.build/repo">
-            Turborepo Docs
-          </Link>
-          <span> | </span>
-          <Link className="App-link" href="https://reactjs.org">
-            React Docs
-          </Link>
-        </div>
-      </header>
+      <h1>Reddit Clone</h1>
     </div>
   )
 }
