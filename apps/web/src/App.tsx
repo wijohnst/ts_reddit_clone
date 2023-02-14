e0ef164c5b00d037a0be758c533639ca6a1fbe15
wijohnst <wijohnst@gmail.com>
2023-02-13 19:41:34 -0500

    chore: Add sass

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,27 +1,8 @@
 import React from "react";
-import { Link } from "ui";
-import "./App.css";
+import "./App.scss";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1 className="header">
-          Web
-          <div className="Turborepo">Turborepo Example</div>
-        </h1>
-        <div>
-          <Link className="App-link" href="https://turbo.build/repo">
-            Turborepo Docs
-          </Link>
-          <span> | </span>
-          <Link className="App-link" href="https://reactjs.org">
-            React Docs
-          </Link>
-        </div>
-      </header>
-    </div>
-  );
+  return <h1 className="App">TS Reddit Clone</h1>;
 }
 
 export default App;
