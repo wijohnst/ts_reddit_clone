8e848ae119368abfc7268f4ca1905dd87f6a6e33
Michael McBride <Michaelmcbridefilms@gmail.com>
2023-02-20 15:08:06 -0500

    fix: fixed res message type

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -7,7 +7,7 @@ const PORT: number = 8000
 app.use(cors())
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Home Rout Working')
+  res.send('Home Route Working')
 })
 
 app.listen(PORT, () => {
