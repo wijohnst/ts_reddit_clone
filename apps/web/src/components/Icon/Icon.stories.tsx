0230a20399fd95e00d6b84578a97c6682f2facef
Michael McBride <Michaelmcbridefilms@gmail.com>
2023-03-06 10:53:29 -0500

    fix: refactor story and component styles

--- apps/web/src/components/Icon/Icon.stories.tsx
+++ apps/web/src/components/Icon/Icon.stories.tsx
@@ -13,34 +13,21 @@ interface IconArgs {
    * child icon SVG component passed to the Icon component
    */
   children: JSX.Element;
-  /**
-   * global css styles applied to all of our Icon components
-   */
-  style?: React.CSSProperties;
 }
-
-const sharedStyles = {
-  width: '100%',
-  height: '100%',
-  margin: '5px',
-};
 
 const Template = (args: IconArgs): JSX.Element => <Icon {...args}></Icon>;
 
 export const Reddit = (args: IconArgs) => <Template {...args} />;
 Reddit.args = {
   children: <RedditIcon color="#ff2800" />,
-  style: sharedStyles,
 };
 
 export const User = (args: IconArgs) => <Template {...args} />;
 User.args = {
-  children: <UserIcon />,
-  style: sharedStyles,
+  children: <UserIcon color="black" />,
 };
 
 export const Home = (args: IconArgs) => <Template {...args} />;
 Home.args = {
-  children: <HomeIcon />,
-  style: sharedStyles,
+  children: <HomeIcon color="black" />,
 };
--- apps/web/src/components/Icon/Icon.tsx
+++ apps/web/src/components/Icon/Icon.tsx
@@ -3,7 +3,6 @@ import './Icon.scss';
 
 interface Props {
   children: React.ReactElement;
-  style?: React.CSSProperties;
 }
 
 /**
@@ -11,11 +10,10 @@ interface Props {
  *
  * @param {Object} props - The component props.
  * @param {React.ReactElement} props.children - The child element to render within the icon.
- * @param {React.CSSProperties} props.style - The inline styles to apply to the icon container element.
  * @returns {JSX.Element} - A JSX element representing the icon component.
  */
-const Icon = ({ children, style }: Props) => {
-  return <div style={style}>{children}</div>;
+const Icon = ({ children }: Props) => {
+  return <div>{children}</div>;
 };
 
 export default Icon;
