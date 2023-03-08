87eecdc3d9f1a5c2cb840660d37670f593b34409
Michael McBride <Michaelmcbridefilms@gmail.com>
2023-03-08 13:52:06 -0500

    feat: update README's Front End section about using svgr

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,7 +1,5 @@
 import * as React from 'react';
 import './App.scss';
-import Icon, { iconColor } from './components/Icon/Icon';
-import { ReactComponent as RedditIcon } from '../src/assets/redditIcon.svg';
 
 const App = (): React.ReactElement => {
   return (
