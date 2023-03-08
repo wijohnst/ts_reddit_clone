a1358d1d01d5b11bb5de4162ad5dac55788a4eb8
Michael McBride <Michaelmcbridefilms@gmail.com>
2023-03-08 13:23:02 -0500

    chore: Remove unnecessary test file

--- apps/web/src/components/Icon/Icon.spec.tsx
+++
@@ -1,8 +0,0 @@
-export {};
-// import React from 'react';
-// import { render, screen } from '@testing-library/react';
-// import Icon from './Icon';
-
-// test('renders without errors', () => {
-//   render(<Icon svg={() => <svg />} />);
-// });
