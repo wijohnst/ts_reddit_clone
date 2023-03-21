c5d3dc83055b2360c6056708b1b4e7270df1f4fc
Michael McBride <Michaelmcbridefilms@gmail.com>
2023-03-21 10:08:25 -0400

    feat: add handleclick function

--- apps/web/src/components/ClickableLogo/ClickableLogo.tsx
+++ apps/web/src/components/ClickableLogo/ClickableLogo.tsx
@@ -4,9 +4,27 @@ import Icon, { iconColor } from '../Icon/Icon';
 import Wordmark from '../Wordmark/Wordmark';
 import { ReactComponent as RedditIcon } from '../../assets/redditIcon.svg';
 
+/**
+ * Renders a logo and wordmark component together within a navbar.
+ *
+ * @returns {React.ReactElement} A JSX element as a clickable reddit logo component.
+ */
+
 const ClickableLogo = (): React.ReactElement => {
+  /**
+   * handles click of logo
+   *
+   * @returns {string} confirming the logo has been clicked
+   *
+   * @description This function is called when the user clicks on the logo. It logs a message to the console to confirm that the click has been registered.
+   */
+
+  const handleClick = () => {
+    console.log('Click Registered');
+  };
+
   return (
-    <div className="logo-container">
+    <div className="logo-container" onClick={() => handleClick}>
       <div className="icon-container">
         <Icon colorProp={iconColor.Orange}>
           <RedditIcon />
