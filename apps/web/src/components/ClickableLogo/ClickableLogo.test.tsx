6256bd82af551107eb97a1849b1aa8a058142dd2
Michael McBride <Michaelmcbridefilms@gmail.com>
2023-03-25 09:45:35 -0400

    refactor: declared mock function at top to avoid reusing code

--- apps/web/src/components/ClickableLogo/ClickableLogo.test.tsx
+++ apps/web/src/components/ClickableLogo/ClickableLogo.test.tsx
@@ -3,23 +3,22 @@ import userEvent from '@testing-library/user-event';
 import ClickableLogo from './ClickableLogo';
 
 describe('ClickableLogo', () => {
+  const handleClick = jest.fn();
+
   // testing for Icon component
   it('should have an Icon component', () => {
-    const handleClick = jest.fn();
     render(<ClickableLogo onClick={handleClick} />);
     expect(screen.getByTestId('clickableicon-icon')).toBeInTheDocument();
   });
 
   // testing for Wordmark component
   it('should have a Wordmark component', () => {
-    const handleClick = jest.fn();
     render(<ClickableLogo onClick={handleClick} />);
     expect(screen.getByTestId('clickableicon-wordmark')).toBeInTheDocument();
   });
 
   // testing that our handleClick fires
   it('should trigger onClick function when the logo is clicked', async () => {
-    const handleClick = jest.fn();
     render(<ClickableLogo onClick={handleClick} />);
     await userEvent.click(screen.getByTestId('clickablelogo'));
     expect(handleClick).toHaveBeenCalled();
