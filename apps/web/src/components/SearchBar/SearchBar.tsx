03f57ddb22108affdfe445c796425b0f4a9f6fe6
Michael McBride <Michaelmcbridefilms@gmail.com>
2023-03-25 15:38:46 -0400

    feat: add searchIcon

--- apps/web/src/components/SearchBar/SearchBar.tsx
+++ apps/web/src/components/SearchBar/SearchBar.tsx
@@ -1,17 +1,19 @@
 import * as React from 'react';
 import { useState } from 'react';
 
-//where does this function live that is being passed?
 interface Props {
+  /** What happens when we use a searchstring to search for results? */
   handleSearch: (searchString: string) => void;
 }
 
-const SearchBar = (props: Props) => {
+// need to add jsdoc comment
+const SearchBar = ({ handleSearch }: Props) => {
   const [searchString, setSearchString] = useState('');
 
+  // need to add jsdoc comment
   const handleKeyDown = (event: React.KeyboardEvent): void => {
     if (event.key === 'Enter') {
-      props.handleSearch(searchString);
+      handleSearch(searchString);
     }
   };
 
