b3ce9c9bca5c975e3d81d0d2bb7941b4ebc8feac
Michael McBride <Michaelmcbridefilms@gmail.com>
2023-03-25 09:39:57 -0400

    feat: install user-event library & refactor onclick test

--- apps/web/src/components/ClickableLogo/ClickableLogo.test.tsx
+++ apps/web/src/components/ClickableLogo/ClickableLogo.test.tsx
@@ -1,27 +1,27 @@
-import { screen, render, fireEvent } from '@testing-library/react';
+import { screen, render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import ClickableLogo from './ClickableLogo';
 
-// testing for Icon component
-it('should have an Icon component', () => {
-  const handleClick = jest.fn();
-  render(<ClickableLogo onClick={handleClick} />);
-  expect(screen.getByTestId('clickableicon-icon')).toBeInTheDocument();
-});
-
-// testing for Wordmark component
-it('should have a Wordmark component', () => {
-  const handleClick = jest.fn();
-  render(<ClickableLogo onClick={handleClick} />);
-  expect(screen.getByTestId('clickableicon-wordmark')).toBeInTheDocument();
-});
-
-// testing that our handleClick fires
 describe('ClickableLogo', () => {
-  it('should trigger onClick function', () => {
+  // testing for Icon component
+  it('should have an Icon component', () => {
     const handleClick = jest.fn();
     render(<ClickableLogo onClick={handleClick} />);
-    const clickablelogo = screen.getByTestId('clickablelogo');
-    fireEvent.click(clickablelogo);
+    expect(screen.getByTestId('clickableicon-icon')).toBeInTheDocument();
+  });
+
+  // testing for Wordmark component
+  it('should have a Wordmark component', () => {
+    const handleClick = jest.fn();
+    render(<ClickableLogo onClick={handleClick} />);
+    expect(screen.getByTestId('clickableicon-wordmark')).toBeInTheDocument();
+  });
+
+  // testing that our handleClick fires
+  it('should trigger onClick function when the logo is clicked', async () => {
+    const handleClick = jest.fn();
+    render(<ClickableLogo onClick={handleClick} />);
+    await userEvent.click(screen.getByTestId('clickablelogo'));
     expect(handleClick).toHaveBeenCalled();
   });
 });
