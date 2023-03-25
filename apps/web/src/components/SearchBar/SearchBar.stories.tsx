a258239ccd916617e858d7c332aeea04ac838c37
Michael McBride <Michaelmcbridefilms@gmail.com>
2023-03-25 17:05:15 -0400

    feat: add story for searchbar

--- apps/web/src/components/SearchBar/SearchBar.stories.tsx
+++ apps/web/src/components/SearchBar/SearchBar.stories.tsx
@@ -1,1 +1,16 @@
-export {};
+import * as React from 'react';
+import SearchBar from './SearchBar';
+
+export default {
+  title: 'Components/SearchBar',
+  component: SearchBar,
+  argTypes: {
+    handleSearch: { action: 'Searching..' },
+  },
+};
+
+interface SearchBarArgs {
+  handleSearch: () => void;
+}
+
+const Template = (args: SearchBarArgs) => <SearchBar {...args} />;
