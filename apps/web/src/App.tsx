4ad07e56040526c763c5ab887e77615177a1420a
Michael McBride <Michaelmcbridefilms@gmail.com>
2023-03-28 18:58:32 -0400

    fix: delete test function

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -2,9 +2,6 @@ import * as React from 'react';
 import './App.scss';
 
 const App = (): React.ReactElement => {
-  const test = () => {
-    console.log('potato');
-  };
   return (
     <div className="App">
       <h1>Reddit Clone</h1>
