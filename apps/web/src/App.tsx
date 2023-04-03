da9985236649e52be8184ee833f7810d70bd9c66
Michael McBride <Michaelmcbridefilms@gmail.com>
2023-04-03 11:31:50 -0400

    feat: add trendingItemSuggestion component

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,10 +1,28 @@
 import * as React from 'react';
 import './App.scss';
+import Icon from './components/Icon/Icon';
+import TrendingItemSuggestion from './components/TrendingItemSuggestion/TrendingItemSuggestion';
+import { ReactComponent as SearchIcon } from '../src/assets/searchIcon.svg';
 
 const App = (): React.ReactElement => {
+  const metaData = {
+    URL: 'Url@URL.com',
+    trendingSuggestionHeading: 'Search Preview Item Heading',
+    trendingSuggestionSummary: 'Search Preview Item Summary',
+    targetSubreddit: {
+      subRedditIcon: (
+        <Icon>
+          <SearchIcon />
+        </Icon>
+      ),
+      subRedditName: 'Dogs',
+    },
+  };
+
   return (
     <div className="App">
       <h1>Reddit Clone</h1>
+      <TrendingItemSuggestion metaData={metaData} />
     </div>
   );
 };
---
+++ apps/web/src/components/TrendingItemSuggestion/TrendingItemSuggestion.tsx
@@ -0,0 +1,47 @@
+import * as React from 'react';
+import { ReactComponent as GoCircleIcon } from '../../assets/goCircleIcon.svg';
+import Icon from '../Icon/Icon';
+
+interface Props {
+  metaData: {
+    URL: string;
+    trendingSuggestionHeading: string;
+    trendingSuggestionSummary: string;
+    targetSubreddit: {
+      subRedditIcon: React.ReactElement;
+      subRedditName: string;
+    };
+  };
+}
+
+const TrendingItemSuggestion = ({ metaData }: Props) => {
+  const {
+    URL,
+    trendingSuggestionHeading,
+    trendingSuggestionSummary,
+    targetSubreddit,
+  } = metaData;
+  const { subRedditIcon, subRedditName } = targetSubreddit;
+
+  return (
+    <li className="list-item">
+      <div className="list-item-text-container">
+        <div className="list-item-heading">
+          <Icon>
+            <GoCircleIcon />
+          </Icon>
+          <h1>{trendingSuggestionHeading}</h1>
+        </div>
+        <h2> {trendingSuggestionSummary}</h2>
+        <div className="list-item-target-subreddit">
+          <Icon>{subRedditIcon}</Icon>
+          <p>{subRedditName}</p>
+        </div>
+      </div>
+      <div className="list-item-preview-image-container">
+        <img src={URL} alt="preview-image" />
+      </div>
+    </li>
+  );
+};
+export default TrendingItemSuggestion;
